297e822d7d3b7b5f792bd156b729177a48d8c278
klgs1234 <138398036+klgs1234@users.noreply.github.com>
2023-08-14 20:38:53 +0000

    tareas

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  // Estados declarados
+  const [tarea, setTarea] = useState("");
+  const [list, setList] = useState([]);
+
+  // Evento al presionar enter en el input
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      setList([...list, tarea]);
+      setTarea("");
+    }
+  };
+
+  // eliminar una tarea de la lista
+  const handleDelete = (index) => {
+    setList(list.filter((_, currentIndex) => index !== currentIndex));
+  };
+
+  return (
+    <div className="container mt-4" >
+      <h1 className="text-center mt-4">todos</h1>
+      <div className="mb-3">
+        <input
+          type="text"
+          className="form-control"
+          onChange={(e) => setTarea(e.target.value)}
+          value={tarea}
+          onKeyPress={handleKeyPress}
+          placeholder="What do you need to do?"
+        />
+      </div>
+      <ul className="list-group">
+        {list.map((item, index) => (
+          <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
+            {item}
+            <button
+              type="button"
+              className="btn-close"
+              aria-label="Close"
+              onClick={() => handleDelete(index)}
+            ></button>
+          </li>
+        ))}
+      </ul>
+      <div className="mt-3">{list.length} item left</div>
+    </div>
+  );
 };
 
 export default Home;
